821ec9ecc2e76f545d461198785a608fc7cd857d
dylangangat <dylan.gangat@gmail.com>
2021-08-26 17:53:41 +0200

    added touch ups to UI for info page and scrollbar

--- info.js
+++ info.js
@@ -30,41 +30,25 @@ getAnimeDetails(animeId);
 // Get anime INFO with session storage and display it
 
 const animeInfoTemplate = info => {
-  const { image_url, title, genres, background, trailer_url } = info;
-
+  const { image_url, title, genres, synopsis, trailer_url } = info;
   const description = `
       <div class="info flow-content">
       <h1 class="main-heading">${title}</h1>
-      <p class="spacer">
-        ${background}
+      <p class="synopsis spacer">
+        ${synopsis}
       </p>
       <div class="tags">
-        <a href="#" class="tag">${genres[0].name}</a>
-        <a href="#" class="tag">${genres[1].name}</a>
-        <a href="#" class="tag">${genres[2].name}</a>
       </div>
       <div class="cta">
         <a href="${trailer_url}" target="_blank" class="btn-secondary btn-secondary--play">
           <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12 0C9.62663 0 7.30655 0.703787 5.33316 2.02236C3.35977 3.34094 1.8217 5.21508 0.913451 7.4078C0.00519945 9.60051 -0.232441 12.0133 0.230582 14.3411C0.693605 16.6689 1.83649 18.807 3.51472 20.4853C5.19295 22.1635 7.33115 23.3064 9.65892 23.7694C11.9867 24.2324 14.3995 23.9948 16.5922 23.0865C18.7849 22.1783 20.6591 20.6402 21.9776 18.6668C23.2962 16.6934 24 14.3734 24 12C23.9965 8.81849 22.731 5.7683 20.4814 3.51863C18.2317 1.26897 15.1815 0.00354552 12 0V0ZM18.4744 12.6486L18.4543 12.6608L9.11889 18.0502L9.09768 18.0625C8.97891 18.1311 8.84418 18.1672 8.70704 18.1672C8.5699 18.1672 8.43517 18.1311 8.31639 18.0626C8.19761 17.994 8.09896 17.8954 8.03036 17.7767C7.96176 17.6579 7.92563 17.5232 7.92558 17.386V6.55814C7.92563 6.421 7.96176 6.28628 8.03036 6.16753C8.09896 6.04877 8.19761 5.95017 8.31639 5.88161C8.43517 5.81306 8.5699 5.77698 8.70704 5.77699C8.84418 5.777 8.97891 5.8131 9.09768 5.88167L9.11889 5.89395L18.4543 11.2833L18.4744 11.2956C18.5931 11.3642 18.6916 11.4629 18.7601 11.5816C18.8286 11.7003 18.8647 11.835 18.8647 11.9721C18.8647 12.1092 18.8286 12.2438 18.7601 12.3626C18.6916 12.4813 18.5931 12.5799 18.4744 12.6486Z"
-              fill="#5928E5"
-            /></svg>Play Trailer
-            </a>
-
+            width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 0C9.62663 0 7.30655 0.703787 5.33316 2.02236C3.35977 3.34094 1.8217 5.21508 0.913451 7.4078C0.00519945 9.60051 -0.232441 12.0133 0.230582 14.3411C0.693605 16.6689 1.83649 18.807 3.51472 20.4853C5.19295 22.1635 7.33115 23.3064 9.65892 23.7694C11.9867 24.2324 14.3995 23.9948 16.5922 23.0865C18.7849 22.1783 20.6591 20.6402 21.9776 18.6668C23.2962 16.6934 24 14.3734 24 12C23.9965 8.81849 22.731 5.7683 20.4814 3.51863C18.2317 1.26897 15.1815 0.00354552 12 0V0ZM18.4744 12.6486L18.4543 12.6608L9.11889 18.0502L9.09768 18.0625C8.97891 18.1311 8.84418 18.1672 8.70704 18.1672C8.5699 18.1672 8.43517 18.1311 8.31639 18.0626C8.19761 17.994 8.09896 17.8954 8.03036 17.7767C7.96176 17.6579 7.92563 17.5232 7.92558 17.386V6.55814C7.92563 6.421 7.96176 6.28628 8.03036 6.16753C8.09896 6.04877 8.19761 5.95017 8.31639 5.88161C8.43517 5.81306 8.5699 5.77698 8.70704 5.77699C8.84418 5.777 8.97891 5.8131 9.09768 5.88167L9.11889 5.89395L18.4543 11.2833L18.4744 11.2956C18.5931 11.3642 18.6916 11.4629 18.7601 11.5816C18.8286 11.7003 18.8647 11.835 18.8647 11.9721C18.8647 12.1092 18.8286 12.2438 18.7601 12.3626C18.6916 12.4813 18.5931 12.5799 18.4744 12.6486Z" fill="#5928E5"/>
+          </svg>Play Trailer</a>
         <a href="#" class="btn-secondary btn-secondary--list">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd"
-          d="M13.0769 11.0769H17.7203C18.1291 11.0769 18.4615 11.4088 18.4615 11.8182V12.4895C18.4615 12.899 18.1297 13.2308 17.7203 13.2308H13.0769V17.8742C13.0769 18.2829 12.7451 18.6154 12.3357 18.6154H11.6643C11.2549 18.6154 10.9231 18.2835 10.9231 17.8742V13.2308H6.27968C5.8709 13.2308 5.53846 12.8989 5.53846 12.4895V11.8182C5.53846 11.4087 5.87032 11.0769 6.27968 11.0769H10.9231V6.43353C10.9231 6.02475 11.2549 5.69231 11.6643 5.69231H12.3357C12.7451 5.69231 13.0769 6.02417 13.0769 6.43353V11.0769ZM0 12C0 5.37258 5.37113 0 12 0C18.6274 0 24 5.37113 24 12C24 18.6274 18.6289 24 12 24C5.37258 24 0 18.6289 0 12Z"
-          fill="#2C3842" />
-      </svg>Add to list</a
-        >
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M13.0769 11.0769H17.7203C18.1291 11.0769 18.4615 11.4088 18.4615 11.8182V12.4895C18.4615 12.899 18.1297 13.2308 17.7203 13.2308H13.0769V17.8742C13.0769 18.2829 12.7451 18.6154 12.3357 18.6154H11.6643C11.2549 18.6154 10.9231 18.2835 10.9231 17.8742V13.2308H6.27968C5.8709 13.2308 5.53846 12.8989 5.53846 12.4895V11.8182C5.53846 11.4087 5.87032 11.0769 6.27968 11.0769H10.9231V6.43353C10.9231 6.02475 11.2549 5.69231 11.6643 5.69231H12.3357C12.7451 5.69231 13.0769 6.02417 13.0769 6.43353V11.0769ZM0 12C0 5.37258 5.37113 0 12 0C18.6274 0 24 5.37113 24 12C24 18.6274 18.6289 24 12 24C5.37258 24 0 18.6289 0 12Z" fill="#2C3842" />
+      </svg>Add to list</a>
       </div>
     </div>
     <div class="image">
@@ -74,6 +58,12 @@ const animeInfoTemplate = info => {
       `;
 
   infoPage.innerHTML = description;
+
+  // Depends on how many genres there are it will populate that amount
+  const tags = document.querySelector(".tags");
+  genres.forEach(
+    genre => (tags.innerHTML += `<a href="#" class="tag">${genre.name}</a>`)
+  );
 };
 
 // Get anime recommendations and add its cards to similar section and addEventListener to get id of recommended card that is clicked and call getAnimeDetails() to repeat all the fucntions again to get new info.
@@ -86,7 +76,8 @@ const getRecommendation = async id => {
 
     if (response.ok) {
       const data = await response.json();
-      const recommendedAnime = data.recommendations.slice(0, 4);
+      console.log(data);
+      const recommendedAnime = data.recommendations.slice(0, 15);
       recommendedList.innerHTML = ""; // added this because when you click on a recommended card it will display the previous 4 and the new 4. So i remove the previous for before getting the new 4.
       recommendedAnime.forEach(recommendedCardTemplate);
       recommendedList.addEventListener("click", e => {
@@ -107,7 +98,6 @@ const getRecommendation = async id => {
 
 const recommendedCardTemplate = show => {
   const { image_url, title, recommendation_count, mal_id } = show;
-
   const card = `
   <div class="anime-card flow-content">
       <div class="image">
